de30288c910a2801807168c2db6df7330320c70d
Shabi Adee <101591738+Shabiblac@users.noreply.github.com>
2023-09-22 09:24:49 +0100

    Modify some styles in the page

---
+++ test.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', () => {
+    const counterElement = document.getElementById('counter');
+    const targetNumber = 100; // The number to count up to
+  
+    let count = 0;
+  
+    const interval = setInterval(() => {
+      if (count <= targetNumber) {
+        counterElement.innerText = count;
+        count++;
+      } else {
+        clearInterval(interval);
+      }
+    }, 50); // Interval in milliseconds
+  });
+  
